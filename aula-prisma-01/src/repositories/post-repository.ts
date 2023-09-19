feabe729191d064461834f67dabe3b3cd79fffc6
hp1heloisa <heloisapimentel2202@gmail.com>
2023-09-18 22:22:04 -0300

    aula1

---
+++ aula-prisma-01/src/repositories/post-repository.ts
@@ -0,0 +1,51 @@
+import db from "../database/database";
+import { Post } from "../protocols/post-protocol";
+
+const TABLE_NAME = "posts";
+
+export type CreatePost = Omit<Post, "id">
+
+async function getPosts() {
+  const result = await db.query<Post>(`
+    SELECT * FROM ${TABLE_NAME} 
+  `);
+
+  return result.rows;
+}
+
+async function getPost(id: number) {
+  const result = await db.query<Post>(`
+    SELECT * FROM ${TABLE_NAME} WHERE id = $1
+  `, [id]);
+
+  return result.rows;
+}
+
+async function createPost(post: CreatePost) {
+  let { username, title, content, createAt} = post;
+  if (createAt == undefined){
+    createAt = new Date();
+  }
+  const result = await db.query<Post>(`
+    INSERT INTO ${TABLE_NAME} (username, title, content, createAt) VALUES ($1, $2, $3, $4)
+  `, [username, title, content, createAt]);
+
+  return result.rowCount;
+}
+
+async function deletePost(id: number) {
+  const result = await db.query<Post>(`
+    DELETE FROM ${TABLE_NAME} WHERE id = $1
+  `, [id]);
+
+  return result.rowCount;
+}
+
+const postRepository = {
+  getPost,
+  getPosts,
+  createPost,
+  deletePost
+}
+
+export default postRepository;
